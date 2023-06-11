4388e2126bf903dee7b85f4ca4b8e6536d843c74
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-11 11:54:59 +0300

    Update Todo.jsx

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,17 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text }) => {
+  
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #1
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button">
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
