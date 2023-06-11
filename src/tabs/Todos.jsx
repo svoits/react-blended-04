fd61cd99c9f4be5bd24ec3cb4d185ebc460be4a2
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-11 11:55:02 +0300

    Update Todos.jsx

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,33 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  }
+
+  addTodo = (text) => {
+    const newTodo = {
+      id: nanoid(),
+      text,
+    }
+    console.log(text);
+    this.setState(prevState => ({
+      todos: [...prevState.todos, newTodo]
+    }))
+  }
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+
+    return (
+      <>
+      <SearchForm onSubmit={this.addTodo} />
+      <Grid>{todos.map(({ id, text }) => (
+        <GridItem key={id}>
+          <Todo text={text} />
+        </GridItem>
+      ))}</Grid>
+      </> 
+    );
   }
 }
