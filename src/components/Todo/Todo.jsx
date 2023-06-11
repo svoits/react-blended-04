d6dc8ca52b1313304c512b69dce943a309cfac38
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-06-11 11:52:10 +0200

    add edit feature

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,18 +1,57 @@
-import { Text } from 'components';
+import { InputSearch, Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { RiDeleteBinLine, RiEdit2Line, RiSave3Fill } from 'react-icons/ri';
+import { Component } from 'react';
 
-export const Todo = ({ text, count, id, onDelete }) => {
-  
-  return (
-    <TodoWrapper>
-      <Text textAlign="center" marginBottom="20px">
-        TODO #{count}
-      </Text>
-      <Text>{text}</Text>
-      <DeleteButton type="button" onClick={() => onDelete(id)}>
-        <RiDeleteBinLine size={24} />
-      </DeleteButton>
-    </TodoWrapper>
-  );
-};
+export class Todo extends Component {
+  state = {
+    isEdit: false,
+    newText: this.props.text,
+  };
+
+  handleSaveEditToggle = () => {
+    if (this.state.isEdit) {
+      this.props.onEdit(this.props.id, this.state.newText);
+    }
+
+    this.setState(prevState => ({
+      isEdit: !prevState.isEdit,
+    }));
+  };
+
+  handleInputChange = e => {
+    this.setState({ newText: e.target.value });
+  };
+
+  render() {
+    const { isEdit, newText } = this.state;
+    const { text, count, id, onDelete } = this.props;
+
+    return (
+      <TodoWrapper>
+        <Text textAlign="center" marginBottom="20px">
+          TODO #{count}
+        </Text>
+        {isEdit ? (
+          <InputSearch
+            placeholder="What do you want to write?"
+            name="edit"
+            required
+            autoFocus
+            value={newText}
+            onChange={this.handleInputChange}
+          />
+        ) : (
+          <Text>{text}</Text>
+        )}
+
+        <DeleteButton type="button" onClick={() => onDelete(id)}>
+          <RiDeleteBinLine size={24} />
+        </DeleteButton>
+        <EditButton type="button" onClick={this.handleSaveEditToggle}>
+          {isEdit ? <RiSave3Fill /> : <RiEdit2Line />}
+        </EditButton>
+      </TodoWrapper>
+    );
+  }
+}
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -6,50 +6,71 @@ const KEYTodos = 'todos';
 export class Todos extends Component {
   state = {
     todos: [],
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     const todosLS = localStorage.getItem(KEYTodos);
-    if(todosLS !== null){
+    if (todosLS !== null) {
       const todos = JSON.parse(todosLS);
-      this.setState({todos});
+      this.setState({ todos });
     }
-    
   }
-  componentDidUpdate(_,prevState){
-    const {todos} = this.state;
-    if(prevState.todos !== todos){
+  componentDidUpdate(_, prevState) {
+    const { todos } = this.state;
+    if (prevState.todos !== todos) {
       localStorage.setItem(KEYTodos, JSON.stringify(todos));
     }
   }
-  addTodo = (text) => {
+  addTodo = text => {
     const newTodo = {
       id: nanoid(),
       text,
-    }
+    };
     console.log(text);
     this.setState(prevState => ({
-      todos: [...prevState.todos, newTodo]
-    }))
+      todos: [...prevState.todos, newTodo],
+    }));
+  };
 
-  }
-  
-  deleteTodo = (id) => {
+  editTodo = (id, newText) => {
     this.setState(prevState => ({
-      todos: prevState.todos.filter( ({id: idTodo}) => idTodo !== id )
-    }))
-  }
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            text: newText,
+          };
+        } else {
+          return todo;
+        }
+      }),
+    }));
+  };
+
+  deleteTodo = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(({ id: idTodo }) => idTodo !== id),
+    }));
+  };
   render() {
     const { todos } = this.state;
 
     return (
       <>
-      <SearchForm onSubmit={this.addTodo} />
-      <Grid>{todos.map(({ id, text }, index) => (
-        <GridItem key={id}>
-          <Todo text={text} id={id} onDelete={this.deleteTodo} count={index+1}/>
-        </GridItem>
-      ))}</Grid>
-      </> 
+        <SearchForm onSubmit={this.addTodo} />
+        <Grid>
+          {todos.map(({ id, text }, index) => (
+            <GridItem key={id}>
+              <Todo
+                text={text}
+                id={id}
+                onDelete={this.deleteTodo}
+                count={index + 1}
+                onEdit={this.editTodo}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
     );
   }
 }
