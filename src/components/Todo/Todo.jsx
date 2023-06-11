84e7be87c4a8e9453d342b91e452db4b270f3b31
talask <35809884+talask@users.noreply.github.com>
2023-06-11 12:24:59 +0300

    todo delete

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,15 +2,15 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = ({ text }) => {
+export const Todo = ({ text, count, id, onDelete }) => {
   
   return (
     <TodoWrapper>
       <Text textAlign="center" marginBottom="20px">
-        TODO #1
+        TODO #{count}
       </Text>
       <Text>{text}</Text>
-      <DeleteButton type="button">
+      <DeleteButton type="button" onClick={() => onDelete(id)}>
         <RiDeleteBinLine size={24} />
       </DeleteButton>
     </TodoWrapper>
