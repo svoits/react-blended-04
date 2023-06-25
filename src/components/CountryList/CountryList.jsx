08c7157d19ce52fd38a39c4e73c3cffacc4d06ae
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-25 12:16:02 +0300

    Update CountryList.jsx

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,13 +1,15 @@
 import { Grid, GridItem } from 'components';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { routes} from 'routes';
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
+
   return (
     <Grid>
       {countries.map(({ id, country, flag }) => {
         return (
           <GridItem key={id}>
-            <Link to={`${routes.COUNTRY}/${id}`}><img src={flag} alt={country} /></Link>
+            <Link to={`${routes.COUNTRY}/${id}`} state={{from: location}}><img src={flag} alt={country} /></Link>
           </GridItem>
         );
       })}
