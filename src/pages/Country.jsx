095cef10599133442ecf23c21265856d70988064
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-25 12:16:05 +0300

    Update Country.jsx

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,8 +1,11 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
+import { Section, Container, CountryInfo, Loader, GoBackBtn } from 'components';
 import { useFetchCountry } from 'hooks/useFetchCountry';
+import { useLocation } from 'react-router-dom';
+import { routes } from 'routes';
 
 export const Country = () => {
   const { country, error, loading } = useFetchCountry();
+  const location = useLocation();
 
   const {
     flag,
@@ -13,9 +16,12 @@ export const Country = () => {
     population,
   } = country ?? {};
 
+  const goBackLink = location?.state?.from ?? routes.HOME;
+
   return (
     <Section>
       <Container>
+        <GoBackBtn path={goBackLink}>Go Back</GoBackBtn>
         {country && (
           <CountryInfo
             flag={flag}
